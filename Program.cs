77e6e6f7cf17651329f1d63a32e491bd6078f2c8
TYTYW29 <a.hayevskyy_fit_4_23_b_d@knute.edu.ua>
2024-05-21 02:15:56 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Numerics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        string inputFilePath = "input.txt";
+        string numberStr = File.ReadAllText(inputFilePath).Trim();
+
+
+        BigInteger number = BigInteger.Parse(numberStr);
+
+
+        int N = 3;
+        BigInteger result = BigInteger.Pow(number, N);
+        Console.WriteLine(result);
+
+    }
+}
